277ab72c9166cbb59fafcc957ef6ae12304c223e
mertcaneser0 <104672923+mertcaneser0@users.noreply.github.com>
2022-05-26 12:05:52 +0300

    Update menü.py

---
+++ final projesi/menü.py
@@ -0,0 +1,12 @@
+from Student import Student
+
+def showMenu():
+    #try: Hataları denetler.
+    try:
+        print("Uludağ Üniversitesi öğrenci tanıma sistemine hoşgeldiniz")
+        print("\n","#"*5, "Seçim Yapın", "#"*5)
+        print("1) Öğrenci ekle")
+        print("2) Öğrenci bul")
+        print("3) Öğrenci sil")
+        print("4) Öğrenci bilgilerini güncelle")
+        print("5) Tüm öğrencileri görüntüle")
