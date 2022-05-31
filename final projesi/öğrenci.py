1e15179b832dd199b99f310cdf68efa84deb4ccd
Berkaykoz <104677686+Berkaykoz@users.noreply.github.com>
2022-05-31 14:25:04 +0300

    Update öğrenci.py

--- final projesi/öğrenci.py
+++ final projesi/öğrenci.py
@@ -13,3 +13,21 @@ class Student:
         # Json formatındaki veriyi sözlüğe çevirir.
         self.count = 0
         null = {}
+try:
+            for i in json.loads(data).keys():
+                if int(i) > self.count:
+                    self.count = int(i)
+            self.count += 1
+        except:
+            return null
+        return json.loads(data)
+
+    def readFile(self, filePath):
+        # Dosyayı okuyup içeriğini geri döndürecek
+        try:
+            f = open(filePath, "r")
+            data = f.read()
+            f.close()
+            return data
+        except FileNotFoundError:
+            return None
